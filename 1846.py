703b9018df8e8b91d2af658f28fd2ef403b03db3
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-07-22 12:17:29 +0530

    Create 1846.py

---
+++ 1846.py
@@ -0,0 +1,12 @@
+#1846. Leetcode Solution in Python
+class Solution(object):
+    def maximumElementAfterDecrementingAndRearranging(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        arr.sort()
+        arr[0] = 1
+        for i in range(1, len(arr)):
+            arr[i] = min(arr[i], arr[i - 1] + 1)
+        return arr[-1]
