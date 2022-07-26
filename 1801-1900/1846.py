580268044f7320d381ac67c4ec631182595f286a
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-07-26 09:21:35 +0530

    Create 1846.py

---
+++ 1801-1900/1846.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maximumElementAfterDecrementingAndRearranging(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        arr.sort()
+        arr[0] = 1
+        for i in range(1, len(arr)):
+            arr[i] = min(arr[i], arr[i - 1] + 1)
+        return arr[-1]
