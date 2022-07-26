6eee30680693e080fee01a1b7954556e8aadaf93
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-07-26 09:51:42 +0530

    Create 19.py

---
+++ 101-500/19.py
@@ -0,0 +1,26 @@
+class Solution:
+    # @return a ListNode
+    def removeNthFromEnd(self, head, n):
+        if n==0 or head==None:
+            return None
+        
+        head1 = ListNode(0)
+        head1.next = head
+        head  = head1
+        
+        p = head    
+        q = head
+        
+        for i in xrange(0,n+1):
+            if q!=None:
+                q=q.next
+            else:
+                return None
+             
+        while q:
+            p=p.next
+            q=q.next
+        
+        p.next = p.next.next
+        
+        return head.next
