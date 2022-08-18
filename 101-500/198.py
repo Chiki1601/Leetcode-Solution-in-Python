9b1edddbe970e38989d4bd1f2f74f08d412d848b
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-18 12:03:11 +0530

    Create 198.py

---
+++ 101-500/198.py
@@ -0,0 +1,13 @@
+
+class Solution(object):
+    # @param num, a list of integer
+    # @return an integer
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        last, now = 0, 0
+        for i in nums:
+            last, now = now, max(last + i, now)
+        return now
