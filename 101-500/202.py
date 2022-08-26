d9c998fb004a55cc16fd269cb8bbef2de3d676dc
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-26 12:52:24 +0530

    Create 202.py

---
+++ 101-500/202.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isHappy(self, n, prev=[]):
+        nums = sum([int(x)**2 for x in list(str(n))])
+        if nums in prev:
+            return False
+        return self.isHappy(nums, prev+[nums]) if nums != 1 else True
+    """
+        :type n: int
+        :rtype: bool
+        """
+        
